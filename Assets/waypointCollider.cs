f73e756718b6c7e11e98659f5ce34ec0b4ba6eec
peter flanagan <peter flanagan>
2017-03-01 23:41:08 +0000

    Get waypoints and UI working

--- Assets/waypointCollider.cs
+++ Assets/waypointCollider.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class waypointCollider : MonoBehaviour {
 
     public bool collided = false;
+    public GameObject waypointController;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,9 +16,18 @@ public class waypointCollider : MonoBehaviour {
 		
 	}
 
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider collider)
     {
-        Debug.Log("Collided");
-        collided = true;
+        if(collider.gameObject.layer == 8)
+        {
+            if(waypointController.GetComponent<waypointsController>().nextWapoint == gameObject)
+            {
+                collided = true;
+            }
+            
+            //Debug.Log("Collided");
+            
+        }
+        
     }
 }
--- Assets/waypointsController.cs
+++ Assets/waypointsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class waypointsController : MonoBehaviour {
 
@@ -9,26 +10,40 @@ public class waypointsController : MonoBehaviour {
     public GameObject nextWapoint;
     public float countDown;
     public float curDown;
+    public Text timeRemaining;
+    public Text totalTime;
+    private float curOverTime = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+        nextWapoint = waypoints[0];
+        curDown = countDown;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        timeRemaining.text = "Time Remaining:"+curDown.ToString("F2");
+        totalTime.text = "Total Time:" + (curOverTime + (countDown - curDown)).ToString("F2");
 		for(int i = 0; i < waypoints.Length; i++)
         {
-            if (!waypoints[i].GetComponent<waypointCollider>().collided)
+            if (i == waypoints.Length-1 && waypoints[i].GetComponent<waypointCollider>().collided)
             {
-                if(nextWapoint != waypoints[i])
+                Debug.Log("Winner");
+            }
+            else if (waypoints[i].GetComponent<waypointCollider>().collided)
+            {
+                if(nextWapoint == waypoints[i])
                 {
-                    nextWapoint = waypoints[i];
+                    nextWapoint = waypoints[i+1];
+                    curOverTime += countDown - curDown;
                     curDown = countDown;
                 }
-                break;
             }
         }
         curDown -= Time.deltaTime;
+        if(curDown <= 0.0f)
+        {
+            Debug.Log("Loser");
+        }
 	}
 }
