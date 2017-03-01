dfab21f6e0f30c6afc48c63b1db3c7fced8d4072
peter flanagan <peter flanagan>
2017-03-01 20:24:47 +0000

    Add waypoints for racers

---
+++ Assets/waypointsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypointsController : MonoBehaviour {
+
+
+    public GameObject[] waypoints;
+    public GameObject nextWapoint;
+    public float countDown;
+    public float curDown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < waypoints.Length; i++)
+        {
+            if (!waypoints[i].GetComponent<waypointCollider>().collided)
+            {
+                if(nextWapoint != waypoints[i])
+                {
+                    nextWapoint = waypoints[i];
+                    curDown = countDown;
+                }
+                break;
+            }
+        }
+        curDown -= Time.deltaTime;
+	}
+}
